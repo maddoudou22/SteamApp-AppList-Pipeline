9fa2b3ab1429ca3a23d6eba3a9f676b4f7035eb5
root <you@example.com>
2018-07-16 13:59:09 +0000

    steamApp-template.yml

---
+++ source/index.js
@@ -0,0 +1,23 @@
+var AWS = require('aws-sdk');
+
+exports.handler = (event, context, callback) => {
+    AWS.config.update({region: 'eu-west-1'});
+    
+    // Recuperation du message envoyé en JSON :
+    var data = JSON.stringify(event);
+    console.log("request: " + data);
+    console.log("prenom: " + event.prenom);
+    console.log("prenom: " + event.nom);
+
+            var responseBody = {
+                "bienvenue": "Bienvenue 2 " + event.prenom + ", " + event.nom
+            };
+    
+            var response = {
+                statusCode: 200,
+                body: responseBody
+            };
+
+            callback(null, response);
+
+};
